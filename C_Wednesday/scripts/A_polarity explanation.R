bf169bed51d3bd81ed7643e309ae472c8add2bd5
kwartler <ehk116@gmail.com>
2019-06-18 15:58:14 -0400

    adj scriptA and fix typos

--- C_Wednesday/scripts/A_polarity explanation.R
+++ C_Wednesday/scripts/A_polarity explanation.R
@@ -13,7 +13,7 @@ polarity("She is doing a very good job.")
 
 # 1 polarized word = "good" = 1
 #6 word cluster of 4 before and 2 after the polarized word
-# cluster neutrals = "he" "is" "doing" "a" "job = 0
+# cluster neutrals = "she" "is" "doing" "a" "job = 0
 # amplifiers = "very" = 0.8
 # total words in passage  = 7
 # here is the result
@@ -24,4 +24,13 @@ polarity("He is doing a good job.")
 #without the amplifier it is 1 amplifier and N is now 6 not 7
 (1) /sqrt(6) #=  0.4082483
 
+polarity("the professor is not very good in lectures but entertaining")
+#ave.polarity = 0.0
+#-1.8 (*not* + very + good) + 1 (entertaining) = 0.8/sqrt(10)
+0.8/sqrt(10) #=.253
+
+polarity("the professor is very good in lectures and entertaining")
+#ave.polarity = 0.0
+#1.8 ( very + good) + 1 (entertaining) = 2.8/sqrt(9)
+2.8/sqrt(9) #=.933
 # End
