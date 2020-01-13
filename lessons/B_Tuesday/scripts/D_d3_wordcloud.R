f326fa927316fbce1c575ec18bb17d8fb0eeaa91
Edward Kwartler <edward.kwartler@edward.kwartler.MacBook>
2020-01-13 22:18:20 +0100

    add Tuesday lesson

---
+++ lessons/B_Tuesday/scripts/D_d3_wordcloud.R
@@ -0,0 +1,96 @@
+#' Title: Text Organization for Bag of Words
+#' Purpose: Learn some basic cleaning functions & term frequency
+#' Author: Ted Kwartler
+#' email: ehk116@gmail.com
+#' License: GPL>=3
+#' Date: 2019-4-29
+#'
+
+# Set the working directory
+setwd("~/Documents/GSERM_TextMining/lessons/B_Tuesday/data")
+
+# Libs
+library(tm)
+library(qdap)
+library(wordcloud2)
+library(RColorBrewer)
+
+# Options & Functions
+options(stringsAsFactors = FALSE)
+Sys.setlocale('LC_ALL','C')
+
+tryTolower <- function(x){
+  y = NA
+  try_error = tryCatch(tolower(x), error = function(e) e)
+  if (!inherits(try_error, 'error'))
+    y = tolower(x)
+  return(y)
+}
+
+cleanCorpus<-function(corpus, customStopwords){
+  corpus <- tm_map(corpus, content_transformer(qdapRegex::rm_url))
+  #corpus <- tm_map(corpus, content_transformer(replace_contraction)) 
+  corpus <- tm_map(corpus, removeNumbers)
+  corpus <- tm_map(corpus, removePunctuation)
+  corpus <- tm_map(corpus, stripWhitespace)
+  corpus <- tm_map(corpus, content_transformer(tryTolower))
+  corpus <- tm_map(corpus, removeWords, customStopwords)
+  return(corpus)
+}
+
+# Create custom stop words
+stops <- c(stopwords('english'), 'lol', 'amp', 'chardonnay')
+
+# Bigram token maker
+bigramTokens <-function(x){
+  unlist(lapply(NLP::ngrams(words(x), 2), paste, collapse = " "), 
+         use.names = FALSE)
+}
+
+# Data
+text <- read.csv('chardonnay.csv', header=TRUE)
+
+# As of tm version 0.7-3 tabular was deprecated
+names(text)[1]<-'doc_id' 
+
+# Make a volatile corpus
+txtCorpus <- VCorpus(DataframeSource(text))
+
+# Preprocess the corpus
+txtCorpus <- cleanCorpus(txtCorpus, stops)
+
+# Make bi-gram TDM according to the tokenize control & convert it to matrix
+wineTDM  <- TermDocumentMatrix(txtCorpus, 
+                               control=list(tokenize=bigramTokens))
+wineTDMm <- as.matrix(wineTDM)
+
+# See a bi-gram
+exampleTweet <- grep('wine country', rownames(wineTDMm))
+wineTDMm[(exampleTweet-2):(exampleTweet),870:871]
+
+# Get Row Sums & organize
+wineTDMv <- sort(rowSums(wineTDMm), decreasing = TRUE)
+wineDF   <- data.frame(word = names(wineTDMv), freq = wineTDMv)
+
+# Regular dynamic WC
+wordcloud2(data = wineDF)
+?wordcloud2
+
+# Choose a color & drop light ones
+pal <- brewer.pal(8, "Dark2")
+wordcloud2(wineDF, color = pal, backgroundColor = "lightgrey")
+
+# Some built in shapes need to click "show in new window"
+# 'circle', 'cardioid', 'diamond', 'triangle-forward', 'triangle', 'pentagon', & 'star'
+wordcloud2(wineDF[1:50,],
+           shape = "cardioid",
+           color = "blue",
+           backgroundColor = "pink")
+
+# Lettercloud and file mask functions do not work w/Chrome therefore not covered.The package is basically orphaned at this point :(
+# https://github.com/Lchiffon/wordcloud2/issues/12
+#letterCloud(wineDF, word = "wine", wordSize = 1)
+#figPath = system.file("examples/t.png",package = "wordcloud2")
+#wordcloud2(wineDF, figPath = figPath, size = 1.5,color = "skyblue")
+
+# End
