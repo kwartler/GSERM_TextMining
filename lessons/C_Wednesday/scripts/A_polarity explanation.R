c4dd3abbfed6f317314ab57e361e19ac272ded81
Edward Kwartler <edward.kwartler@edward.kwartler.MacBook>
2020-01-14 22:09:41 +0100

    add weds

---
+++ lessons/C_Wednesday/scripts/A_polarity explanation.R
@@ -0,0 +1,37 @@
+#' Title: Polarity Cluster
+#' Purpose: Explore polarity calc
+#' Author: Ted Kwartler
+#' email: ehk116@gmail.com
+#' License: GPL>=3
+#' Date: Jan-14-2020
+#'
+
+library(qdap)
+polarity("She is doing a very good job.")
+#ave.polarity = 0.68
+
+
+# 1 polarized word = "good" = 1
+#6 word cluster of 4 before and 2 after the polarized word
+# cluster neutrals = "she" "is" "doing" "a" "job = 0
+# amplifiers = "very" = 0.8
+# total words in passage  = 7
+# here is the result
+(1 + 0.8) / sqrt(7) #= 0.6803361
+
+polarity("He is doing a good job.")
+#ave.polarity = .408
+#without the amplifier it is 1 amplifier and N is now 6 not 7
+(1) /sqrt(6) #=  0.4082483
+
+polarity("the professor is not very good in lectures but entertaining")
+#ave.polarity = 0.25
+#-1.8 (*not* + very + good) + 1 (entertaining) = 0.8/sqrt(10)
+0.8/sqrt(10) #=.253
+
+polarity("the professor is very good in lectures and entertaining")
+#ave.polarity = 0.0
+#1.8 ( very + good) + 1 (entertaining) = 2.8/sqrt(9)
+2.8/sqrt(9) #=.933
+# End
+
---
+++ lessons/C_Wednesday/scripts/E_kmediods.R
@@ -0,0 +1,68 @@
+#' Title: K Means
+#' Purpose: apply k-mediods
+#' Author: Ted Kwartler
+#' email: ehk116@gmail.com
+#' License: GPL>=3
+#' Date: Jan-14-2020
+#' https://cran.r-project.org/web/packages/kmed/vignettes/kmedoid.html
+
+# Wd
+setwd("/cloud/project/C_Wednesday/data")
+
+# Libs
+library(kmed)
+library(tm)
+library(clue)
+library(cluster)
+library(wordcloud)
+
+# Bring in our supporting functions
+source('/cloud/project/Z_otherScripts/ZZZ_plotCluster.R')
+source('/cloud/project/Z_otherScripts/ZZZ_supportingFunctions.R')
+
+# Options & Functions
+options(stringsAsFactors = FALSE)
+Sys.setlocale('LC_ALL','C')
+
+# Stopwords
+stops <- c(stopwords('SMART'), 'work')
+
+# Read & Preprocess
+txtMat <- cleanMatrix(pth = '1yr_plus_final4.csv', 
+                      columnName  = 'text', # clue answer text 
+                      collapse = F, 
+                      customStopwords = stops, 
+                      type = 'DTM', 
+                      wgt = 'weightTfIdf') #weightTf or weightTfIdf
+
+# Remove empty docs w/TF-Idf
+txtMat <- subset(txtMat, rowSums(txtMat) > 0)
+
+# Use a manhattan distance matrix; default for kmed
+manhattanDist <- distNumeric(txtMat, txtMat, method = "mrw")
+
+# Calculate the k-mediod
+txtKMeds <- fastkmed(manhattanDist, ncluster = 5, iterate = 5)
+
+# Number of docs per cluster
+table(txtKMeds$cluster)
+barplot(table(txtKMeds$cluster), main = 'k-mediod')
+
+# Visualize separation
+plotcluster(manhattanDist, txtKMeds$cluster, pch = txtKMeds$cluster)
+
+# Two silhouette functions...kmed
+silKMed <- kmed::silhoutte(manhattanDist, txtKMeds$medoid, txtKMeds$cluster)
+
+# Examine results
+silKMed$result[c(1:3,22:26, 47:50),]
+silKMed$plot
+
+# Two silhouette functions...kmed
+silPlot <- cluster::silhouette(txtKMeds$cluster, manhattanDist)
+plot(silPlot, border=NA)
+
+# Median centroid documents:
+txtKMeds$medoid
+
+# End
